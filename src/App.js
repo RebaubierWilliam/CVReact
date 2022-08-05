90c9e93b5d5f5c1fb487d6e2aa9bdaad3cea6ce9
William Rebaubier <RebaubierWilliam@gmail.com>
2022-08-05 18:04:29 +0200

    2 eme archivage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.scss';
+import Sidebar from './component/Sidebar';
 
 function App() {
   return (
-    <div>
+    <div className='main__conteneur'>
+      {/* sidebar  */}
+      <Sidebar />
+      <div className='main'>Main</div>
+
     </div>
   );
 }
---
+++ src/component/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Sidebar.scss';
+
+function Sidebar() {
+    return (
+        <div className='sideBar__container'>
+            <img className='photo' src='../images/ma photo jpg.jpg' alt='' />
+            <h2>William Rebaubier</h2>
+        </div>
+    )
+}
+
+export default Sidebar
